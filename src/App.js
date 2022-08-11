196f36d937ac81c19bb76bff2ffb467a4748c7fd
TataKor <107408600+TataKor@users.noreply.github.com>
2022-08-11 15:10:46 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 import "./styles.css";
-import Search from "./Search.1";
 
 
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
       <h1>Weather today</h1>
-      <Search />
       </header>
     </div>
     );
 }
-
-export default App;
