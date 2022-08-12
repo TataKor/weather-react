66e8aaeddf16f486ecc5c58af4275f879bb0aa5f
TataKor <107408600+TataKor@users.noreply.github.com>
2022-08-12 11:50:33 +0300

    build search form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,34 @@ export default function App() {
     <div className="App">
       <header className="App-header">
       <h1>Weather today</h1>
+      <div className="row">
+          <input
+            type="search"
+            id="town"
+            placeholder="Enter a city"
+            autoFocus="on"
+            className="search-input"
+          />
+          <input
+            type="submit"
+            id="search"
+            className="btn search"
+            value="Search"
+          />
+          <br />
+          <h2>
+            In Kyiv is 23 °C.
+          </h2>
+          <div className="col-6 metrics-description showTemperature">
+          <ul>
+            Description: sunny.
+            <br />
+            Humidity: 15 %.
+            <br />
+            Wind: 2 m/s.
+          </ul>
+        </div>
+        </div>
       </header>
     </div>
     );
